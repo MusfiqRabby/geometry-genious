30add6a3012d76ff1a553bc60583a64cd5e03d76
musfiqrabby <musfiqrabby69@gmail.com>
2024-12-27 19:32:58 +0600

    functionality added

--- scripts/triangle-calculate.js
+++ scripts/triangle-calculate.js
@@ -1,7 +1,20 @@
 
 
+
+// get triangle base value 
 function calculateArea(){
-	console.log('inside function')
+
+const triagnleBase = document.getElementById('triangle-base')
+const triangleText = triagnleBase.value;
+const base = parseFloat(triangleText)
+// console.log(base)
+
+// get triangle height value
+const triangleHeight = document.getElementById('triangle-height');
+const triangleText2 = triangleHeight.value;
+const height = parseFloat(triangleText2)
+console.log(height)
+
 }
 
 
